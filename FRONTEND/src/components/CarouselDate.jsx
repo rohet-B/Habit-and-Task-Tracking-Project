7cf1991bf552790a66766cd3a8f1868bf03d889d
Rohit B <rohetbiswash@gmail.com>
2026-02-26 07:21:00 +0530

    Daisy UI

---
+++ FRONTEND/src/components/CarouselDate.jsx
@@ -0,0 +1,60 @@
+import React from 'react'
+
+const CarouselDate = () => {
+    const today = new Date(); 
+    const year = today.getFullYear();
+    const month = today.getMonth() + 1; // 0-11 (Jan = 0)
+    const day = today.getDate();
+    const total_days = new Date(year,month,0).getDate();
+    // 0 means here:
+        // Go to the 1st day of the given month (here 2, since we have added 1 in month_var and in js 2 means march)
+        // Then go back 1 day back 
+        // Result = Last day of the previous month which is the total days
+  return (
+    <div className='flex items-center justify-center mt-3.5'>
+        <div className='carousel w-150 h-10 flex items-center gap-8'>
+            <button className='cursor-pointer carousel-item bg-slate-900 text-slate-50 rounded-xl h-10 min-w-7 px-2 flex justify-center items-center'>
+                {day} out of {total_days} for month {month}, {year}
+            </button>   
+            <div className='carousel-item bg-slate-900 text-slate-50 rounded-xl h-10 min-w-7 px-2 flex justify-center items-center'>
+                72
+            </div>   
+            <div className='carousel-item bg-slate-900 text-slate-50 rounded-xl h-10 min-w-7 px-2 flex justify-center items-center'>
+                73
+            </div>   
+            <div className='carousel-item bg-slate-900 text-slate-50 rounded-xl h-10 min-w-7 px-2 flex justify-center items-center'>
+                74
+            </div>   
+            <div className='carousel-item bg-slate-900 text-slate-50 rounded-xl h-10 min-w-7 px-2 flex justify-center items-center'>
+                75
+            </div>   
+            <div className='carousel-item bg-slate-900 text-slate-50 rounded-xl h-10 min-w-7 px-2 flex justify-center items-center'>
+                76
+            </div>   
+            <div className='carousel-item bg-slate-900 text-slate-50 rounded-xl h-10 min-w-7 px-2 flex justify-center items-center'>
+                77
+            </div>   
+            <div className='carousel-item bg-slate-900 text-slate-50 rounded-xl h-10 min-w-7 px-2 flex justify-center items-center'>
+                78
+            </div>   
+            <div className='carousel-item bg-slate-900 text-slate-50 rounded-xl h-10 min-w-7 px-2 flex justify-center items-center'>
+                79
+            </div>   
+            <div className='carousel-item bg-slate-900 text-slate-50 rounded-xl h-10 min-w-7 px-2 flex justify-center items-center'>
+                80
+            </div>   
+            <div className='carousel-item bg-slate-900 text-slate-50 rounded-xl h-10 min-w-7 px-2 flex justify-center items-center'>
+                81
+            </div>   
+            <div className='carousel-item bg-slate-900 text-slate-50 rounded-xl h-10 min-w-7 px-2 flex justify-center items-center'>
+                82
+            </div>   
+            <div className='carousel-item bg-slate-900 text-slate-50 rounded-xl h-10 min-w-7 px-2 flex justify-center items-center'>
+                83
+            </div>   
+    </div>
+    </div>
+  )
+}
+
+export default CarouselDate
--- FRONTEND/src/pages/Home/HomePage.jsx
+++ FRONTEND/src/pages/Home/HomePage.jsx
@@ -1,12 +1,42 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import CarouselDate from '../../components/CarouselDate';
 
 const HomePage = () => {
+        // const currentDate = new Date();
+        // const dateString = currentDate.toLocaleDateString();
+        // const timeString = currentDate.toLocaleTimeString();
+
+    const [dateString,setCurrentDate] = useState();
+    const [timeString,setTimestring] = useState();
+
+    const currentGreetings = () =>{
+        const today = new Date();
+        const options = {
+            weekday: 'long',
+            year: 'numeric',
+            month: 'long',
+            day: 'numeric',
+        }
+        setCurrentDate(today.toLocaleDateString("en-IN",options));
+
+        if (today.getHours()<12){
+            setTimestring("Good Morning");
+        }
+        else{
+            setTimestring("Good Evening");
+        }
+    }
+
+    useEffect(()=>{
+        currentGreetings()
+    },[])
+    
   return (
     <>
         <div className='h-fit w-full bg-slate-800 flex justify-center p-3 items-center gap-25 xl:gap-200'>
             <div className='text-slate-50 text-lg font-bold'>
-                <div>Good Evening, Rohit</div>
-                <div className='text-slate-400'>Wednesday, 10 February 2025</div>
+                <div>{timeString}, Rohit</div> 
+                <div className='text-slate-400'>{dateString}</div>
             </div>
             <div >
                 <button className='cursor-pointer text-4xl text-amber-200'>
@@ -14,6 +44,7 @@ const HomePage = () => {
                 </button>
             </div>
         </div>
+        <CarouselDate/>
     </>
   )
 }
